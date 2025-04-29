ea0455d8a6224af9d5e395b55d946e0a606cee9b
ead <ead@academico.local>
2025-04-29 08:26:28 -0300

    Atualizacoes

--- jtransfer/aula1/src/Aluno.java
+++ jtransfer/aula1/src/Aluno.java
@@ -1,5 +1,4 @@
 
-
 // encapsulamento
 // public
 // private
@@ -7,42 +6,39 @@
 // package
 
 public class Aluno {
-    
+
     // variaveis = atributos
     // caracteristicas
-    
+
     private String nome; // atributo do tipo String
     private int idade; // atributo do tipo Numero
     private boolean temFaculdade; // atributo do tipo Booleano (0 e 1)
     // classes wrappers
+
     public static void main(String[] args) throws Exception {
-    
-        
-        
-    
+
     }
 
     public String getNome() { // metodo para pegar o nome
         return nome;
     }
+
     public int getIdade() { // metodo para pegar a idade
         return idade;
     }
+
     public boolean getTemFaculdade() { // metodo para pegar temFaculdade
         return temFaculdade;
     }
 
-    public Aluno(){
+    public Aluno() {
 
     }
 
-    public Aluno(String nome, int idade, boolean temFaculdade){
+    public Aluno(String nome, int idade, boolean temFaculdade) {
         this.nome = nome;
         this.idade = idade;
         this.temFaculdade = temFaculdade;
     }
-
-
-
 
 }
